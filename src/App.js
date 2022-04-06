7b4793e545bce21b00fbf22776002846038d1eb8
mehtab ahmed <ahmedmehtab176@gmail.com>
2022-04-07 00:21:43 +0200

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import React from "react";
 
 function App() {
+  const [cards, setcards] = React.useState([1, 2,34,67, 3, 4, 56, 7]);
+  const cardsList = cards.map((item,index) => {
+    return (
+      <div class="card">
+        <div class={`container ${(index===3 || index===4) ? "large-cards" : ""}`}  id={index}>
+          <h4>
+            <b>John Doe</b>
+          </h4>
+          <p>Architect & Engineer</p>
+        </div>
+      </div>
+    );
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Hello</header>{" "}
+      <div className="body">
+
+      </div>
+      <div style={{display:'flex', justifyContent:'right',flexDirection:'row', flexWrap:'wrap',width:'800px'}}>
+      {cardsList}
+      </div>
+      
     </div>
   );
 }
